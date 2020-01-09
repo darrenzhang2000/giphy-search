556edae00dbb86a173914c3047f026e1dfb4af1b
Darren Zhang <darrenzhang200027@gmail.com>
2020-01-09 14:08:25 -0500

    able to find 1 img url

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 
 class SearchField extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            testImg: null
+        }
+    }
+
+    handleSearchInput = () => {
+        let linkToAPI = "http://api.giphy.com/v1/gifs/search?q=" + "cats" + "&api_key=sqPHtsBm3ol63E2X1iIJRktBKkzxe4qZ";
+        //console.log(linkToAPI);
+        fetch(linkToAPI)
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                let data = myJson;
+                console.log(data.data[0].url);
+                this.setState({testImg: data.data[0].url})
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
     render(){
-        return <input type="text" placeholder="Try Cats"/>
+        return <div>
+            <input type="text" placeholder="Try Cats" onChange={this.handleSearchInput}/>
+            <img src={this.state.testImg}/>
+            <p>{this.state.testImg}</p>
+        </div>
     }
 }
 
